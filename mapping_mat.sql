4688e59c9cafa10058faa6484257b1c491d37ee4
Farhan Ali <Farhan.Ali@emaratech.ae>
2016-11-30 16:44:28 +0400

    changes to commit

--- mapping_mat.sql
+++ mapping_mat.sql
@@ -1,3 +1,8 @@
 insert into mapping_matrix_item (ID, COL_1, COL_2, COL_3, COL_4, COL_5, ACTIVE, COL_7, COL_8, COL_9, COL_6, COL_10, MAPPING_MATRIX_ID, COL_11, COL_12, COL_13, COL_14, COL_15, COL_16, COL_17, COL_18, COL_19, COL_20, COL_21, COL_22, COL_23, COL_24, COL_25, COL_26, COL_27, COL_28, COL_29, COL_30, EXCLUDE)
 values (3846, '200', '', '', '', '2000', 1, '', '1', '', '', '3', 1, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', null);
 
+
+sdsad
+
+waoo
+
